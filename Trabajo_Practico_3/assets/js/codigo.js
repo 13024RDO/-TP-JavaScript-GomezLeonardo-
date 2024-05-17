3cba97c551721a9656b758bda046ffc1b415b615
Leonardo <leoonardoogzz@gmail.com>
2024-05-17 10:12:39 -0600

    se creo la primera funcion

---
+++ Trabajo_Practico_3/assets/js/codigo.js
@@ -0,0 +1,18 @@
+let operacion = parseInt(prompt(
+    "Lista de tareas\n" +
+    "1- Ver lista de tareas\n" +
+    "2- Agregar Tareas\n" +
+    "3- Editar Tarea\n" +
+    "4- Eliminar Tareas"
+  ));
+
+  let tareas = [];
+  
+  switch (operacion) {
+    case 1:
+        alert(tareas)
+        break;
+  
+    default:
+        break;
+  }
