f6bd9244db95e2b43493dcc967dce58c4e9e6998
Leonardo <leoonardoogzz@gmail.com>
2024-05-20 09:12:19 -0600

    Se creo la funcion agregar

--- Trabajo_Practico_3/assets/js/codigo.js
+++ Trabajo_Practico_3/assets/js/codigo.js
@@ -1,18 +1,46 @@
-let operacion = parseInt(prompt(
-    "Lista de tareas\n" +
-    "1- Ver lista de tareas\n" +
-    "2- Agregar Tareas\n" +
-    "3- Editar Tarea\n" +
-    "4- Eliminar Tareas"
-  ));
+let tareas = [];
+let operacion = 0;
 
-  let tareas = [];
-  
-  switch (operacion) {
-    case 1:
-        alert(tareas)
-        break;
-  
-    default:
-        break;
-  }
+while (operacion !== 5) {
+    operacion = parseInt(prompt(
+        "Lista de tareas\n"
+         +
+        "1- Ver lista de tareas\n" +
+        "2- Agregar Tareas\n" +
+        "3- Editar Tarea\n" +
+        "4- Eliminar Tareas\n" +
+        "5- Salir"
+    ));
+
+    switch (operacion) {
+        case 1:
+            VerTareas();
+            break;
+        case 2:
+            AgregarTarea();
+            break;
+        case 3:
+            EditarTarea();
+            break;
+        case 4:
+            EliminarTarea();
+            break;
+        case 5:
+            console.log("Saliendo del programa...");
+            break;
+        default:
+            console.log("Opción no válida. Por favor, elija una opción entre 1 y 5.");
+    }
+}
+
+
+function AgregarTarea() {
+    let tarea = prompt("Agrega una tarea:");
+    if (tarea && tarea.trim()) {
+        tareas.push(tarea);
+        console.log("Tarea agregada:", tarea);
+    } else {
+        console.log("No se agregó ninguna tarea. La entrada estaba vacía.");
+    }
+}
+
